585df9f236512d47e33d27622e4b2193ca6f5978
AleXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-05-09 15:40:19 -0400

    new

--- js/script.js
+++ js/script.js
@@ -1,55 +1,81 @@
 var listSave = [
   {
-    direc: "tabs/Sparkle.pdf",
+    direc_pdf: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbnRvZHJjS2FvcjdyUEhTYVZpNDVxLXZKLTBXd3xBQ3Jtc0tsRDN1dEhZT3lOSW5kOFZ4ZmJDX05IVTE2LWxKYUtHS0tDOGxwUmNQWE9NTVU2d1lxSWY0eUk3aFFMLURmWldXSi05b2gza3JfQ3dZZjUxbWoxcGQ4YzhpdUk5WFFLbnZ1UU1vaEx6VXpKdUVXWW5Maw&q=https%3A%2F%2Fdrive.google.com%2Fopen%3Fid%3D1MlsMaZDOhHnSGgdFOqF-m0SuHWMjUnFi&v=rQmkw6OqxbA",
+    direc_youtube: "https://www.youtube.com/watch?v=rQmkw6OqxbA",
+    direc_channel: "https://www.vvxofingerstyletab.com/",
     description: "Sparkle-Radwimps",
     gender: "Anime",
     creator: 0,
   },
   {
-    direc: "tabs/KATAWARE-DOKI.pdf",
+    direc_pdf: "https://drive.google.com/file/d/1uhiwBUpNK1gzZVzeO45nDgZdMO9B_XQO/view?usp=sharing",
+    direc_youtube: "https://www.youtube.com/watch?v=WHZzYJ1-aM0",
+    direc_channel: "https://www.vvxofingerstyletab.com/",
     description: "Kataware Doki-Radwimps",
     gender: "Anime",
     creator: 0,
   },
   {
-    direc: "tabs/suzume-no-tojimari.pdf",
+    direc_pdf: "https://drive.google.com/file/d/1P--v7NOLHHuM7G6deQDBMsfSNB63y27V/view?usp=sharing",
+    direc_youtube: "https://www.youtube.com/watch?v=mj4Pf-seQWQ",
+    direc_channel: "https://www.vvxofingerstyletab.com/",
     description: "Suzume-Radwimps",
     gender: "Anime",
     creator: 0,
   },
   {
-    direc: "tabs/The-Weight-of-the-World.pdf",
-    description: "Weight of the world-Nier Automata",
-    gender: "Game",
-    creator: 0,
-  },
-  {
-    direc: "tabs/City-Ruins.pdf",
-    description: "City Ruins-Nier Automata",
-    gender: "Game",
-    creator: 0,
-  },
-  {
-    direc: "tabs/i-really-want-to-stay-at-your-house.pdf",
+    direc_pdf: "https://drive.google.com/file/d/1lnSGjOuNUR-EqWGQkN7DvbqRWLjfUstC/view?usp=sharing",
+    direc_youtube: "https://www.youtube.com/watch?v=yUka8hbTivY",
+    direc_channel: "https://www.vvxofingerstyletab.com/",
     description: "I really want to stay at your house-CyberPunk EdgeRunners",
     gender: "Anime",
     creator: 0,
   },
   {
-    direc: "tabs/chainsaw-man-kick-back.pdf",
+    direc_pdf: "https://drive.google.com/file/d/18khP-0q2tTGFztAzJC7KWzSH2zgQT9-i/view?usp=share_link",
+    direc_youtube: "https://www.youtube.com/watch?v=F4g0quH7TKY",
+    direc_channel: "https://www.vvxofingerstyletab.com/",
     description: "Kick back-Chainsaw Man",
     gender: "Anime",
     creator: 0,
   },
   {
-    direc: "tabs/Coffin-Dance-TABS.pdf",
+    direc_pdf: "https://drive.google.com/file/d/1nYJ6QBueh4dZ7-oxDBd_GznkXOfMw1Yj/view?usp=sharing",
+    direc_youtube: "https://www.youtube.com/watch?v=icjP6K-4kJ8",
+    direc_channel: "https://www.vvxofingerstyletab.com/",
     description: "Astronomia (Coffin Dance Meme Song)",
     gender: "POP",
     creator: 0,
   },
   {
-    direc: "tabs/Song-of-the-Ancients.pdf",
+    direc_pdf: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbjZMb2doS19nZTdITDNvZHZzN3UzVk9NZG16QXxBQ3Jtc0trMW42azVJLVNkNTNIeWdrMmJPYUtWNXIyZzdyX0h0WUl1UDJGMFBwOVZmM1h2RHBaa3JTZHhQbUw0Qk5zT3BjUlM2Y0I1WGFXbW1HeDQ1emtyMjhoUUJxWVpyUUlxOWR5T1c4VDFRSVAxc0lCVmRLaw&q=http%3A%2F%2Fwww.mediafire.com%2Ffile%2F5uv6jp7ah7tk7x3%2FSong_of_the_Ancients.gpx&v=WncPZV2C1Mo",
+    direc_youtube: "https://www.youtube.com/watch?v=WncPZV2C1Mo",
+    direc_channel: "",
     description: "Songs of the Ancients-Nier Replicant",
+    gender: "Game",
+    creator: 0,
+  },
+  {
+    direc_pdf: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbDZfLVI4SmpLMzlkVGhGRGtqRkwyUjBHczFNd3xBQ3Jtc0trMTRCc3kybE9vcU50TFJHVE90REdhYzZwUC0weHpZYTBlc0FaYkt1b3g2WDhjME5kUEozVnlQZUlYSmV4Z1FDSTNjVW1xVm1TbFJZaHpkV0VRekhLeGU3bzliUDdhZXZScWNOczJkRF81bC1EVEZFaw&q=http%3A%2F%2Fwww.mediafire.com%2Ffile%2Ff2y9emdu86u9y8h%2FKimi_no_Na_wa_-_Nandemonaiya.pdf%2Ffile&v=ETKWW53d_-4",
+    direc_youtube: "https://www.youtube.com/watch?v=ETKWW53d_-4",
+    direc_channel: "",
+    description: "Nandemonaiya-Radwimps",
+    gender: "Anime",
+    creator: 0,
+  },
+  {
+    direc_pdf: "",
+    direc_youtube: "https://www.youtube.com/watch?v=ETKWW53d_-4",
+    direc_channel: "",
+    description: "Mix of 4 songs-Nier Automata",
+    gender: "Game",
+    creator: 0,
+  },
+  {
+    direc_pdf: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbHBKaFY2cGd4MmVtZFFYNDhPQWtjdlBubVVBQXxBQ3Jtc0trZjRLNHRQYWI1Rl9OLXFiQm0xNTR4ZWN5bE9qNlNUVW9MS2tPOVFlVDJqSEFaWFpkVG9JWHNKak5saGdKclBPMzBWRWxYM3VGUjJhWUZsMnBHWEx5MDlJdlYzMnQxWU5pTFhwTC1haGtoZV8zRGdHTQ&q=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1gxPUqFRmyZOIgQ_gOz6N4oPYsh-CfGEQ%2Fview%3Fusp%3Ddrivesdk&v=OVGFjosVjPc",
+    direc_youtube: "https://www.youtube.com/watch?v=OVGFjosVjPc",
+    direc_channel: "",
+    description: "Shikisai-SPYxFAMILY ED 2",
     gender: "Game",
     creator: 0,
   },
@@ -139,15 +165,28 @@ function showList() {
   for (var i = 12 * pos; i < list.length && count < 12; i++) {
     count++;
     text += `
-    <a class="${
+    <div class="item ${
       i == 0 + 12 * pos
         ? "first"
         : i == list.length - 1 || (pos + 1) * 12 - 1 == i
         ? "last"
         : ""
-    }" href=${list[i].direc}>${i + 1}- ${
+    }" >${i + 1}- ${
       list[i].description
-    }</a>
+    } <a href='${list[i].direc_pdf}' ${list[i].direc_pdf==""?'hidden':''}><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
+  </svg></a>
+  <a href='${list[i].direc_youtube}' ${list[i].direc_youtube==""?'hidden':''}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+</svg>
+  </a>
+  <a href='${list[i].direc_channel}' ${list[i].direc_channel==""?'hidden':''}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
+</svg>
+  </a>
+    </div>
         `;
   }
   document.getElementById("collection").innerHTML = text;
